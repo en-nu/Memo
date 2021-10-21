ae04a10d8119638efa004fc0b83f2e9382cf7ac0
en-nu <59733371+en-nu@users.noreply.github.com>
2021-10-21 09:18:28 +0900

    Create Memo Screens

--- App.jsx
+++ App.jsx
@@ -3,12 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEdisScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 
 
 export default function App() {
   return (
-    <MemoListScreen />
+    <MemoCreateScreen />
 
   );
 }
